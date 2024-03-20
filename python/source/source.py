54e7b2c6269f96b521f27c4948411a67323d1165
soysaucecrab <88924237+soysaucecrab@users.noreply.github.com>
2024-03-20 21:23:55 +0900

    dd

--- python/source/source.py
+++ python/source/source.py
@@ -66,75 +66,127 @@ def rectangle():
     # Hide the sketch
     # sketch.isVisible = False
 
-def truss(n, w, h, th):
-    #init
-    # Get the active Fusion 360 application
-    app = adsk.core.Application.get()
-    if not app:
-        raise RuntimeError("Failed to get Fusion 360 application")
+def truss_down(n, w, h, th):
+    try :
+        #init
+        # Get the active Fusion 360 application
+        app = adsk.core.Application.get()
+        if not app:
+            raise RuntimeError("Failed to get Fusion 360 application")
+        ui = app.userInterface
+        # Get the root component of the active design
+        design = app.activeProduct
+        root_comp = design.rootComponent
+        if not root_comp:
+            raise RuntimeError("Failed to get root component")
 
-    # Get the root component of the active design
-    design = app.activeProduct
-    root_comp = design.rootComponent
-    if not root_comp:
-        raise RuntimeError("Failed to get root component")
+        # Create a new sketch on the XY plane of the root component
+        sketches = root_comp.sketches
+        xz_plane = root_comp.xZConstructionPlane
+        sketch = sketches.add(xz_plane)
+        if not sketch:
+            raise RuntimeError("Failed to create sketch")
+        
+        #int
+        lined = []
+        lines = sketch.sketchCurves.sketchLines
+        curvesToOffset = adsk.core.ObjectCollection.create()
 
-    # Create a new sketch on the XY plane of the root component
-    sketches = root_comp.sketches
-    xz_plane = root_comp.xZConstructionPlane
-    sketch = sketches.add(xz_plane)
-    if not sketch:
-        raise RuntimeError("Failed to create sketch")
-    
-    #int
-    lined = []
-    lines = sketch.sketchCurves.sketchLines
-    
-    ##this is for outside lines
-    #down
-    start_point = adsk.core.Point3D.create(0, 0, 0)
-    end_point = adsk.core.Point3D.create(w, 0, 0)
-    line = lines.addByTwoPoints(start_point, end_point)
-    #up
-    start_point = adsk.core.Point3D.create((w/n)/2, -h, 0)
-    end_point = adsk.core.Point3D.create(w-(w/n)/2, -h, 0)
-    line = lines.addByTwoPoints(start_point, end_point)
-    lined.append(line)
-    #other truss lines - outside
-    for i in range(n):
-        start_point = adsk.core.Point3D.create((w/n)*i, 0, 0)
-        end_point = adsk.core.Point3D.create((w/n)*i+(w/n)/2, -h, 0)
+        ##this is for outside lines
+        #down
+        start_point = adsk.core.Point3D.create(w, 0, 0)
+        end_point = adsk.core.Point3D.create(0, 0, 0)
         line = lines.addByTwoPoints(start_point, end_point)
         lined.append(line)
-    for i in range(n):
-        start_point = adsk.core.Point3D.create((w/n)*(i+1), 0, 0)
-        end_point = adsk.core.Point3D.create((w/n)*i+(w/n)/2, -h, 0)
-        line = lines.addByTwoPoints(start_point, end_point)
-        lined.append(line)
-    
+        #other truss lines - outside
+        for i in range(n):
+            start_point = adsk.core.Point3D.create((w/n)*i, 0, 0)
+            end_point = adsk.core.Point3D.create((w/n)*i+(w/n)/2, -h, 0)
+            line1 = lines.addByTwoPoints(lined[i*2].endSketchPoint, end_point)
+            lined.append(line1)
+            start_point = adsk.core.Point3D.create((w/n)*i+(w/n)/2, -h, 0)
+            end_point = adsk.core.Point3D.create((w/n)*(i+1), 0, 0)
+            line2 = lines.addByTwoPoints(lined[i*2+1].endSketchPoint, end_point)
+            lined.append(line2)
+        
+        #uping
+        # end_point = adsk.core.Point3D.create(w-(w/n)/2, -h, 0)
+        # line3 = lines.addByTwoPoints(lined[2*n].endSketchPoint, end_point)
+        # lined.append(line3)
+        # #uped
+        # end_point = adsk.core.Point3D.create((w/n)/2, -h, 0)
+        # line4 = lines.addByTwoPoints(lined[2*n+1].endSketchPoint, end_point)
+        # lined.append(line4)
 
-    ##this is for inside lines
-    #other truss lines - inside
-    for i in range(n):
-        start_point = adsk.core.Point3D.create((w/n)*i+(th*w)/(h*n), -(th*w)/(2*h*n), 0)
-        end_point = adsk.core.Point3D.create((w/n)*i+(w/n)/2, -h+(th*w)/(h*n), 0)
+        curves = sketch.findConnectedCurves(line)
+               
+        # Create the offset.
+        dirPoint = adsk.core.Point3D.create(0, -0.5, 0)
+        offsetCurves = sketch.offset(curves, dirPoint, -th)
+
+    except:
+        if ui:
+            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
+
+def truss_up(n, w, h, th):
+    try :
+        #init
+        # Get the active Fusion 360 application
+        app = adsk.core.Application.get()
+        if not app:
+            raise RuntimeError("Failed to get Fusion 360 application")
+        ui = app.userInterface
+        # Get the root component of the active design
+        design = app.activeProduct
+        root_comp = design.rootComponent
+        if not root_comp:
+            raise RuntimeError("Failed to get root component")
+
+        # Create a new sketch on the XY plane of the root component
+        sketches = root_comp.sketches
+        xz_plane = root_comp.xZConstructionPlane
+        sketch = sketches.add(xz_plane)
+        if not sketch:
+            raise RuntimeError("Failed to create sketch")
+        
+        #int
+        lined = []
+        lines = sketch.sketchCurves.sketchLines
+        curvesToOffset = adsk.core.ObjectCollection.create()
+
+        ##this is for outside lines
+        #down
+        start_point = adsk.core.Point3D.create(w-(w/n)/2, -h, 0)
+        end_point = adsk.core.Point3D.create((w/n)/2, -h, 0)
         line = lines.addByTwoPoints(start_point, end_point)
         lined.append(line)
-    for i in range(n):
-        start_point = adsk.core.Point3D.create((w/n)*(i+1)-(th*w)/(h*n), -(th*w)/(2*h*n), 0)
-        end_point = adsk.core.Point3D.create((w/n)*i+(w/n)/2, -h+(th*w)/(h*n), 0)
-        line = lines.addByTwoPoints(start_point, end_point)
-        lined.append(line)
-    for i in range(n):
-        start_point = adsk.core.Point3D.create((w/n)*i+th, -(th*w)/(2*h*n), 0)
-        end_point = adsk.core.Point3D.create((w/n)*(i+1)-th, -(th*w)/(2*h*n), 0)
-        line = lines.addByTwoPoints(start_point, end_point)
-        lined.append(line)
+        #other truss lines - outside
+        for i in range(n-1):
+            end_point = adsk.core.Point3D.create((w/n)*(i+1), 0, 0)
+            line1 = lines.addByTwoPoints(lined[i*2].endSketchPoint, end_point)
+            lined.append(line1)
+            end_point = adsk.core.Point3D.create((w/n)*(i+1)+(w/n)/2, -h, 0)
+            line2 = lines.addByTwoPoints(lined[i*2+1].endSketchPoint, end_point)
+            lined.append(line2)
+
+        curves = sketch.findConnectedCurves(line)
+               
+        # Create the offset.
+        dirPoint = adsk.core.Point3D.create(0, 0.5, 0)
+        offsetCurves = sketch.offset(curves, dirPoint, -th)
+    except:
+        if ui:
+            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
+        
 
 
+
+def truss(n,w,h,th):
+    truss_up(n,w,h,th)
+    truss_down(n,w,h,th)
 
 def main():
-    truss(5,20,2,0.2)
+    truss(5,20,2,0.25)
 
 def run(context):
     try:
